4e2475b15e396eed9533904366be8dfd86b966cb
Christopher Bisom <chris@infosentience.com>
2024-04-05 12:55:40 -0400

    quick fix to test deployment

--- src/app/projects/music/page.tsx
+++ src/app/projects/music/page.tsx
@@ -10,7 +10,7 @@ export default function Music() {
         think are worthwhile.
       </p>
       <p>
-        I've included any background context along with recordings on the
+        I have included any background context along with recordings on the
         individual music project pages. My hope is to upload scores (and maybe
         better recording quality) in the not-too-distant-future.
       </p>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,9 +4,9 @@ export default function Projects() {
   return (
     <DirectoryPageWrapper header="Projects">
       <p>
-        I'm always working on something or other, although I keep most of this
+        I am always working on something or other, although I keep most of this
         work fairly private. In any case, this directory contains some projects
-        I'm happy enough to share.
+        I am happy enough to share.
       </p>
     </DirectoryPageWrapper>
   );
