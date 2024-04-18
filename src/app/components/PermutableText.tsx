84f8d0325af1771db43876648d3e21e9c21a9d8c
Christopher Bisom <chris@infosentience.com>
2024-04-18 16:47:01 -0400

    Move dragging code into the react structure; update permutabletext to ensure non-repeats; slight code cleanup

--- src/app/components/PermutableText.tsx
+++ src/app/components/PermutableText.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import { shuffleString } from "../scripts/wordAnimations";
 import styles from "./PermutableText.module.css";
 
@@ -8,17 +8,46 @@ interface PermutableTextProps {
   delay?: number;
 }
 
+// Fisher-Yates
+export function shuffle<T>(array: T[]): T[] {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
 const PermutableText = ({ text, options, delay = 60 }: PermutableTextProps) => {
   const [isPermuting, setIsPermuting] = useState<Boolean>(false);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useEffect(
+    // On rerender, we shuffle the options
+    () => {
+      options = shuffle(options);
+    },
+    []
+  );
 
   const handleClick: MouseEventHandler = (e) => {
     if (isPermuting) return; // Don't try to permute the text while already permuting or it could get garbled
     setIsPermuting(true);
-    shuffleString(
-      e.target as HTMLElement,
-      options[Math.floor(Math.random() * options.length)],
-      delay,
-      () => setIsPermuting(false)
+    // Iterate through the options
+    setCurrentIndex((currentIndex + 1) % options.length);
+    shuffleString(e.target as HTMLElement, options[currentIndex], delay, () =>
+      setIsPermuting(false)
     );
   };
 
---
+++ src/app/draggableGrid.ts
@@ -0,0 +1,2 @@
+// TODO: Move somewhere else. This file used to have more in it.
+export const DRAG_RESIZE_EVENT_NAME = "drag-resize";
